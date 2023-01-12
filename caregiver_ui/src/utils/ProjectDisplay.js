1224d022517dd91d1584480e656d1eb719cd8708
Matthew Rada <matthewr7.christopher@gmail.com>
2023-01-12 15:51:00 -0500

    finished carousel

--- caregiver_ui/src/utils/ProjectDisplay.js
+++ caregiver_ui/src/utils/ProjectDisplay.js
@@ -14,8 +14,6 @@ import step11 from "../images/step11.PNG"
 import step12 from "../images/step12.PNG"
 import step13 from "../images/step13.PNG"
 
-
-
 import ProjectDisplayStyles from "./ProjectDisplayStyles"
 import Container from "react-bootstrap/Container"
 import Card from 'react-bootstrap/Card'
@@ -37,106 +35,93 @@ const ProjectDisplay = () => {
         <Card>
 
           {index === 0 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 1: Home Page </Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              The home page allows the user to view a list of caregivers, clients, and timestamps.
             </Card.Text>
           </Card.Body>}
 
           {index === 1 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 2: View Clients</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User may add a client, edit a client's info, and view further details. This same process applies to caregivers.
             </Card.Text>
           </Card.Body>}
 
           {index === 2 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 3: View Caregivers</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              When viewing caregivers, the user may view their hours as well as add and modify them. In this case we are viewing Melissa Yulo's timestamps.
             </Card.Text>
           </Card.Body>}
 
           {index === 3 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 4: Add Timestamp</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can then add a timestamp indicating who the client is, time in/out, and hourly rate charged.
             </Card.Text>
           </Card.Body>}
 
           {index === 4 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 5: Process Timestamps</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can make as many records as needed for the week.
             </Card.Text>
           </Card.Body>}
 
           {index === 5 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 6: Process Timestamps</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can then select timestamps to process.
             </Card.Text>
           </Card.Body>}
 
           {index === 6 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 7: Preview and Confirm</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A preview is created and confirmation is needed.
             </Card.Text>
           </Card.Body>}
 
           {index === 7 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 8: Additional Information</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can then add additional information and after confirming results, a record is created. 
             </Card.Text>
           </Card.Body>}
 
           {index === 8 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 9: View Records</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              After creating a record, it is added to a list to identify if compensation was recieved.
             </Card.Text>
           </Card.Body>}
 
           {index === 9 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 10: View a Record</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can then access the created record.
             </Card.Text>
           </Card.Body>}
 
           {index === 10 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 11: Update Record</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              User can then update that compensation has been recieved as well as print the document if needed.
             </Card.Text>
           </Card.Body>}
 
           {index === 11 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 12: Record Updated</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              Compensation has been recieved, user can then update the record.
             </Card.Text>
           </Card.Body>}
 
           {index === 12 && <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Step 13: View All Timestamps</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              All timestamps can be viewed and reviewed for the convenience of the user.
             </Card.Text>
           </Card.Body>}
 
